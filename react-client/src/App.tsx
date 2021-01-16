2a0fc55d5e73fd26091c5681fbac7e29fb3f653b
Simon Pichugin <simon.pichugin@gmail.com>
2021-01-16 18:43:45 +0100

    Add Mobile Ant Design node_module

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,17 +1,44 @@
 import React, { useState } from 'react';
+import { Tabs, Badge } from 'antd-mobile';
 import './App.css';
+
 
 function App() {
     import('wasm').then(({ print_string }) => {
-    const strResult = print_string();
+    const strResult = print_string("Hello");
     //const strResult = "World";
     setStr(strResult);
   })
   const [strFile, setStr] = useState("");
+  const tabs = [
+    { title: <Badge text={'13'}>First Tab</Badge> },
+    { title: <Badge text={'® Test'}>Second Tab</Badge> },
+    { title: <Badge dot>Third Tab</Badge> },
+  ];
+
+  const TabExample = () => (
+    <div>
+      <Tabs tabs={tabs}
+        initialPage={1}
+        onChange={(tab, index) => { console.log('onChange', index, tab); }}
+        onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
+      >
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '200px', backgroundColor: '#fff' }}>
+          {strFile} - Content of first tab
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '200px', backgroundColor: '#fff' }}>
+          Content of second tab
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '200px', backgroundColor: '#fff' }}>
+          Content of third tab
+        </div>
+      </Tabs>
+    </div>
+  );
 
   return (
     <div className="App" >
-      <div>String Results: {strFile}</div>
+        <TabExample />
     </div>
   );
 }
--- wasm-foundryvt-pb/src/lib.rs
+++ wasm-foundryvt-pb/src/lib.rs
@@ -1,12 +1,11 @@
 use wasm_bindgen::prelude::*;
 
-//       
+//
 // TODO: 1. React waits for POST from the server
 //       2. Rust parses and calculates the data to be printed
 //       3. React draws the interface and continues to wait for another request
 #[wasm_bindgen]
-pub fn print_string() -> String {
+pub fn print_string(greeting: String) -> String {
     let foo_bar = "World";
-    return format!("Hello {}", foo_bar);
+    return format!("{} {}", greeting, foo_bar);
 }
-
